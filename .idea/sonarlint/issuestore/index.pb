cc454f92062b2e298c5bf3d1b02435044edae8aa
Edgar Cole <edgarcolejr@gmail.com>
2021-02-15 19:55:28 -0800

    Wow, I think I'm done! I just need to add the documentation.

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -1,3 +1,9 @@
 
 Z
 *src/EDU/UCSD/Extension/Lesson3Streams.java,c\9\c96625c0782f283caf6c00d62051396e652332f7
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+D
+JobResult_124432.txt,e\8\e88416409a950370feda0c41a3c2af6992b0cf04
+G
+Lesson-3-Assignment.iml,6\d\6d9d65047079674425b72e4b227467364163ef70
--- src/EDU/UCSD/Extension/Lesson3Streams.java
+++ src/EDU/UCSD/Extension/Lesson3Streams.java
@@ -3,19 +3,24 @@ package EDU.UCSD.Extension;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Pattern;
+
 
 public class Lesson3Streams {
     private String filename = null;
     private String inputData = null;
-    // The default pattern matches a 16-digit hexadecimal number that begins
-    // with 8 leading zeros.
-    private String regex = "[0]{8}[0-9a-f]{8}";
+    private String regex;
 
     /**
      * @param filename
      */
     public Lesson3Streams(String filename) {
         this.filename = filename;
+        // The default pattern matches a 16-digit hexadecimal number that begins
+        // with 8 leading zeros.
+        this.regex = "[0]{8}[0-9a-f]{8}";
     }
 
     /**
@@ -34,6 +39,26 @@ public class Lesson3Streams {
         inputData = new String(Files.readAllBytes(Paths.get(filename)));
     }
 
+    public int getStringSize() throws NullPointerException {
+        return this.inputData.length();
+    }
+
+    public long countTheOccurrencesUsingStream() {
+        Pattern pattern = Pattern.compile(regex);
+        List<String> words = Arrays.asList(inputData.split("\\b"));
+        return words.stream()
+                .filter(pattern.asPredicate())
+                .count();
+    }
+
+    public long countTheOccurrencesUsingParallelStream() {
+        Pattern pattern = Pattern.compile(regex);
+        List<String> words = Arrays.asList(inputData.split("\\b"));
+        return words.parallelStream()
+                .filter(pattern.asPredicate())
+                .count();
+    }
+
     /**
      *
      */
@@ -41,16 +66,82 @@ public class Lesson3Streams {
         System.out.println(inputData);
     }
 
+    public void doubleDown() throws IOException {
+        inputData = inputData + new String(Files.readAllBytes(Paths.get(filename)));
+    }
+
     public static void main(String[] arguments) throws IOException {
         String fileName = null;
+        String regex = "[0]{8}[0-9a-f]{8}";
+        long count = 0, startTime = 0, endTime = 0, singleThreaded = 0, multiThreaded = 0;
+
         try {
             fileName = arguments[0];
         } catch (ArrayIndexOutOfBoundsException arrayIndexOutOfBoundsException) {
             System.err.println("Oops! This program requires an argument.");
             arrayIndexOutOfBoundsException.printStackTrace();
         }
-        Lesson3Streams lesson3Streams = new Lesson3Streams(fileName);
+
+        Lesson3Streams lesson3Streams = new Lesson3Streams(fileName, regex);
         lesson3Streams.readTheFile();
-        lesson3Streams.dumpTheContents();
+        singleThreaded = endTime - startTime;
+        System.out.println("Try 1:");
+        System.out.printf("String size: %d%n", lesson3Streams.getStringSize());
+
+        startTime = System.currentTimeMillis();
+        count = lesson3Streams.countTheOccurrencesUsingStream();
+        endTime = System.currentTimeMillis();
+        singleThreaded = endTime - startTime;
+
+        System.out.printf("Milliseconds using stream: %,.2f%n", (double) singleThreaded);
+
+        startTime = System.currentTimeMillis();
+        count = lesson3Streams.countTheOccurrencesUsingParallelStream();
+        endTime = System.currentTimeMillis();
+        multiThreaded = endTime - startTime;
+
+        System.out.printf("Milliseconds using parallelStream: %,.2f%n", (double) multiThreaded);
+
+        if (multiThreaded < singleThreaded) {
+            System.out.printf("Results: ParallelStream was %,.2f milliseconds " +
+                            "faster than the single-threaded stream.%n",
+                    (double) (singleThreaded - multiThreaded));
+        } else if (singleThreaded < multiThreaded) {
+            System.out.printf("Results: stream was %,.2f milliseconds faster than the parallelStream.%n",
+                    (double) (multiThreaded - singleThreaded));
+        } else {
+            System.out.println("Results: No difference.");
+        }
+
+        System.out.println("Doubling String size and trying again…");
+
+        lesson3Streams.doubleDown();
+
+        System.out.println("Try 2:");
+        System.out.printf("String size: %d%n", lesson3Streams.getStringSize());
+
+        startTime = System.currentTimeMillis();
+        count = lesson3Streams.countTheOccurrencesUsingStream();
+        endTime = System.currentTimeMillis();
+
+        System.out.printf("Milliseconds using stream: %,.2f%n", (double) singleThreaded);
+
+        startTime = System.currentTimeMillis();
+        count = lesson3Streams.countTheOccurrencesUsingParallelStream();
+        endTime = System.currentTimeMillis();
+        multiThreaded = endTime - startTime;
+
+        System.out.printf("Milliseconds using parallelStream: %,.2f%n", (double) multiThreaded);
+
+        if (multiThreaded < singleThreaded) {
+            System.out.printf("Results: ParallelStream was %,.2f milliseconds " +
+                            "faster than the single-threaded stream.%n",
+                    (double) (singleThreaded - multiThreaded));
+        } else if (singleThreaded < multiThreaded) {
+            System.out.printf("Results: stream was %,.2f milliseconds faster than the parallelStream.%n",
+                    (double) (multiThreaded - singleThreaded));
+        } else {
+            System.out.println("Results: No difference.");
+        }
     }
 }
